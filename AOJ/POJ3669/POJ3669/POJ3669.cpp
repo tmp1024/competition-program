97ea3415daf63d4e10e8f8ebafbcb8675edc84af
ShotaSato <satou.inct@gmail.com>
2014-08-08 11:04:55 +0900

    POJ3669 1回目の提出

---
+++ AOJ/POJ3669/POJ3669/POJ3669.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cstdio>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+const int INF = 1 << 12;
+
+int M, X, Y;
+int field[302][302];
+int d[302][302];
+
+int dx[] = { 1, 0, -1, 0 };
+int dy[] = { 0, 1, 0, -1 };
+
+typedef pair<int, int> P;
+
+int bfs()
+{
+	queue<P> que;
+
+	for (int i = 0; i < 302; i++){
+		for (int j = 0; j < 302; j++){
+			d[i][j] = INF;
+		}
+	}
+
+	que.push(P(1, 1));
+	d[1][1] = 0;
+
+	while (!que.empty()){
+		P p = que.front(); que.pop();
+
+		if (field[p.first][p.second] == INF) return d[p.first][p.second];
+
+		for (int i = 0; i < 4; i++){
+			int nx = p.first + dx[i], ny = p.second + dy[i];
+
+			if (0 < nx && nx < 302 && 0 < ny && ny < 302 && field[nx][ny] - 1 > d[p.first][p.second] && d[nx][ny] == INF){
+				que.push(P(nx, ny));
+				d[nx][ny] = d[p.first][p.second] + 1;
+				cout << nx << " " << ny << " " << d[nx][ny] << endl;
+			}
+		}
+	}
+
+	return -1;
+}
+
+int main()
+{
+	int dx[] = { 0, 1, 0, -1, 0 };
+	int dy[] = { 0, 0, 1, 0, -1 };
+
+	for (int i = 0; i < 302; i++){
+		for (int j = 0; j < 302; j++){
+			field[i][j] = INF;
+		}
+	}
+
+	cin >> M;
+
+	for (int i = 0; i < M; i++){
+		int X, Y, T;
+
+		cin >> X >> Y >> T;
+
+		X++, Y++;
+
+		for (int j = 0; j < 5; j++){
+			int nx = X + dx[j], ny = Y + dy[j];
+
+			field[nx][ny] = T;
+			//cout << nx << " " << ny << " " << T << endl;
+
+		}
+	}
+
+	cout << bfs() << endl;
+
+	return 0;
+}
